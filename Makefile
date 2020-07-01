8cdac7b9cf57c2c94e90ce8afe257579f2c0a5bc
AntonioSoriani <mandalore05@gmail.com>
2020-07-01 11:34:51 -0300

    Rushing to package everything and simplify the code

--- Makefile
+++ Makefile
@@ -1,28 +1,34 @@
 
 
 
-
-OPT = -march=x86-64 -msse2 \
+# MATH = -mfpmath=387 -mno-sse -mno-sse2 -mno-mmx -mno-3dnow
+MATH = -msse2
+OPT = -march=x86-64 -mcpu=x86-64 \
+-msse2\
 -save-temps=obj \
 -flto -Wl,-O1 -g -O2
 benchmark: main.cpp clear
+#	g++ -o benchmark.o.gcc \
+#		-march=x86-64 -mcpu=x86-64 -mfpmath=387 -mno-sse -mno-sse2 -mno-mmx -mno-3dnow -mno-sse -std=c++1z\
+#		-g -Og main.cpp
 	clang++-10 -o benchmark.s -c -S -masm=intel \
 		$(OPT) -Wall -pedantic -std=c++1z main.cpp && \
-	g++ -o benchmark.o.gcc \
-		-march=x86-64 -msse2 -std=c++1z\
-		-g -Og main.cpp && \
 	clang++-10 -o benchmark \
 		$(OPT) main.cpp && \
 	clang++-10 -o benchmark.o.clang.0 \
-		-march=x86-64 -msse2 -g -O0 main.cpp && \
+		-march=x86-64 -mcpu=x86-64 \
+		$(MATH)	-g -O0 main.cpp && \
 	clang++-10 -o benchmark.o.clang.1 -std=c++1z \
-		-march=x86-64 -msse2 -g -O1 main.cpp && \
+		-march=x86-64 -mcpu=x86-64 \
+		$(MATH)	-g -O1 main.cpp && \
 	clang++-10 -o benchmark.o.clang.2 -std=c++1z \
-		-march=x86-64 -msse2 -g -O2 main.cpp && \
+		-march=x86-64 -mcpu=x86-64 \
+		$(MATH)	-g -O2 main.cpp && \
 	clang++-10 -o benchmark.o.clang.3 -std=c++1z \
-		-march=x86-64 -msse2 -g -O3 -Wall -pedantic main.cpp
+		-march=x86-64 -mcpu=x86-64 \
+		$(MATH)	-g -O3 -Wall -pedantic main.cpp
 	objdump -dSCs -Mintel benchmark > benchmark.ss
-	objdump -dSCs -Mintel benchmark.o.gcc > benchmark.ssgcc
+#	objdump -dSCs -Mintel benchmark.o.gcc > benchmark.ssgcc
 	objdump -dSCs -Mintel benchmark.o.clang.0 > benchmark.ss0
 	objdump -dSCs -Mintel benchmark.o.clang.0 > benchmark.ss1
 	objdump -dSCs -Mintel benchmark.o.clang.0 > benchmark.ss2
--- main.cpp
+++ main.cpp
@@ -1,24 +1,106 @@
 #include <iostream>
+#include <limits>
+#include <cmath>
 
 
-double gregory_leibniz(const unsigned int &N){
+#ifdef X87MATH
+// x86-64 FORCES the use of SSE registers for return values.
+// Since we want to use x87 code for minimal space usage,
+// we have to return the result of functions using a global variable.
+#define return_double(x) ret_val = x
+#define return_double_t void
+static double ret_val = 93;
+
+#else
+
+#define return_double(x) return x;
+#define return_double_t double
+#endif
+
+
+// We need intrinsics in order to issue a RDTSC instruction
+// (which reads the system clock) from the C++ side.
+// It's called a unsigned long long __rdtsc(void) from here.
+#ifdef _WIN32
+#include <intrin.h>
+#else
+#include <x86intrin.h>
+#endif
+
+// Global variable for the measured time
+unsigned long long delta_t;
+
+
+return_double_t gauss_legendre(const unsigned int &N){
+    unsigned long long first_read = __rdtsc();
+    unsigned long long last_read = 0;
+
+    double a = 1;
+    double b = 1 / std::sqrt(2);
+    double s = 1 / 2;
+    double p = 1;
+
+    std::cout << "a: " << a << "\nb: " << b << "\ns: " << s << "\n";
+    for(int i=0; i<N; i++){
+        double last_a = a;
+        a = (a + b) / 2;
+        b = std::sqrt(last_a * b);
+        double c2 = (a - last_a) * (a - last_a);
+        s = s - (std::pow(2, i + 1) * c2);
+        std::cout << "Gauss-Legendre iter " << i << ": " << (a + b) * (a + b) / (4 * s) << "\n";
+        std::cout << "a: " << a << "\nb: " << b << "\ns: " << s << "\n";
+    }
+
+    double ret_val = (a + b) * (a + b) / (2 * s);
+
+    delta_t = last_read - first_read;
+    return_double(ret_val);
+}
+
+
+return_double_t gregory_leibniz(const unsigned int &N){
     double sign = +1;
     double over = 1;
     double acc = 0;
+    unsigned long long first_read = __rdtsc();
+    unsigned long long last_read = 0;
     for(int i=0; i<N; i++){
         // 1/over is 1/1, 1/3, 1/5, 1/7, 1/9...
         // sign is +1, -1, +1, -1, +1, -1, +1...
         acc += (double) sign * (1 / over);
         sign *= -1;
         over += 2;
+        last_read = __rdtsc();
     }
-    return acc * 4;
+    delta_t = last_read - first_read;
+    return_double(acc * 4);
 }
 
 
+#ifndef X87MATH
 int main(){
+    std::cout.precision(std::numeric_limits<double>::max_digits10);
     for(int i=0; i<10; i++){
-        std::cout << gregory_leibniz(4 * 1024 * 1024) << "\n";
+        std::cout << delta_t << " cycles -- [Gregory Leibniz] "
+                  << gregory_leibniz(4 * 1024 * 1024)  << "\n";
+        std::cout << delta_t << " cycles -- [Gauss Legendre] "
+                  << gauss_legendre(4 * 1024 * 1024)  << "\n";
     }
     return 0;
 }
+
+#else
+
+int main(){
+    ret_val = ret_val / 10;
+
+    for(int i=0; i<10; i++){
+        ret_val += 1;
+
+        std::cout << ret_val  << "\n";
+        gregory_leibniz(4 * 1024 * 1024);
+        std::cout << delta_t << " cycles -- " << ret_val  << "\n";
+    }
+    return 0;
+}
+#endif
